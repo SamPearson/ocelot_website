e81c983e0c9a6fb899bff75ebed5652b89e95f25
Sam Pearson <sam.ed.pearson@gmail.com>
2026-08-20 20:54:50 -0500

    Playwright tests

---
+++ tests/tests/smoke-test.spec.ts
@@ -0,0 +1,26 @@
+import {test, expect} from 'playwright/test'
+
+const base_url = process.env.BASE_URL;
+
+
+test('load index page', async ({page}) => {
+
+    await page.goto('/');
+    await expect( page.getByRole('heading', {name: 'Ocelot Code Systems'})).toBeVisible()
+
+})
+
+test('load blog page', async ({page}) => {
+
+    await page.goto('/blog');
+    await expect( page.getByRole('heading', {name: 'The Dev Blog'})).toBeVisible()
+
+})
+
+test('navigate to blog page', async({page}) => {
+    await page.goto('/')
+    await page.getByRole('link', {name: 'blog'}).click()
+
+    await expect( page.getByRole('heading', {name: 'The Dev Blog'})).toBeVisible()
+
+})
